d2defa8811054501e581e46f762b6de3a842ff85
sebastianceloch <73536373+sebastianceloch@users.noreply.github.com>
2022-05-18 16:02:40 +0200

    Add files via upload

---
+++ lab11/kod.pl
@@ -0,0 +1,35 @@
+zwierze(zaba).
+zwierze(traszka).
+zwierze(ropucha).
+zwierze(aksolotl).
+/*tu mamy plazy*/
+zwierze(waz).
+zwierze(jaszczurka).
+zwierze(zolw).
+zwierze(krokodyl).
+zwierze(aligator).
+/*tu gady*/
+zwierze(mrowka).
+zwierze(patyczak).
+zwierze(modliszka).
+zwierze(lisciec).
+/*tu mamy owady*/
+zwierze(pajak).
+zwierze(skorpion).
+/*tu mamy bezkregowce*/
+
+/*gatunki zwierzat*/
+gatunek(zaba,rogata).
+gatunek(zaba,szponiasta).
+gatunek(traszka,grzebieniasta).
+gatunek(traszka,japonska).
+gatunek(ropucha,aga).
+gatunek(ropucha,azjatycka).
+gatunek(waz,zbozowy).
+gatunek(waz,mahoniowy).
+gatunek(waz,boa_dusiciel).
+gatunek(jaszczurka,agama_brodata).
+gatunek(jaszczurka,gekon).
+
+
+
